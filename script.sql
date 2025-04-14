9e3615285b1260e283c7cfd65e0fe6a20181a6f5
Luiz <miguel342613@gmail.com>
2025-04-14 19:45:52 -0300

    criando o banco de dados

---
+++ script.sql
@@ -0,0 +1,24 @@
+-- CRIANDO O BANCO DE DADOS
+create database bdEcommerce;
+
+-- USANDO O BANCO DE DADOS
+use bdEcommerce;
+
+-- CRIANDO AS TABELAS
+create table Usuario(
+	Id int primary key auto_increment,
+    Nome varchar(50) not null,
+    Email varchar(50) not null,
+    Senha varchar(50) not null
+);
+
+create table Cliente(
+	CodCli int primary key auto_increment,
+    NomeCli varchar(50) not null,
+    TelCli varchar(20) not null,
+    EmailCli varchar(50) not null
+);
+
+-- CRIANDO AS CONSULTAS
+select * from Usuario;
+select * from Cliente;
